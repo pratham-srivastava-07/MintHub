9a783353a46f93568a50b4d585c0e00124949101
Krish <prathamrajsri2003@gmail.com>
2024-11-01 21:56:42 +0530

    response

--- components/layout/Appbar.tsx
+++ components/layout/Appbar.tsx
@@ -1,34 +1,36 @@
-
+"use client"
 import { signIn, useSession } from "next-auth/react";
 import { SiLaunchpad } from "react-icons/si";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import UserAuth from "../content/UserAuth";
 
-
-
 export default function Appbar() {
-    const {data: session} = useSession()
-    return <div>
-        <div className="flex justify-between items-center p-4 pt-7 sm:gap-3">
-            <div className="title flex justify-center items-center space-x-3 pl-10">
-                <SiLaunchpad />
-                <Link href={"/"}><div className="font-bold text-2xl cursor-pointer">Minthub</div></Link>
-            </div>
-            <div className="signin flex justify-center items-center gap-7 pr-10">
-
-                    <div className="about">
-                        <Link href={"/about"} className="hover:underline"><Button variant={"outline"}>About</Button></Link>
-                    </div>
-
-                    {!session ?
-                         <Link href={"/api/auth/signin"}>
-                            <Button variant={"outline"} onClick={()=> signIn("email", {callbackUrl: "/select", redirect: false})}>Signin</Button>
+    const { data: session } = useSession();
+    return (
+        <div className="p-4 pt-7">
+            <div className="flex justify-between items-center">
+                <div className="title flex items-center space-x-2 sm:space-x-3 pl-4 sm:pl-10">
+                    <SiLaunchpad />
+                    <Link href={"/"}>
+                        <div className="font-bold text-2xl cursor-pointer">Minthub</div>
+                    </Link>
+                </div>
+                <div className="signin flex items-center gap-4 sm:gap-7 pr-4 sm:pr-10">
+                    <Link href={"/about"} className="hover:underline">
+                        <Button variant={"outline"}>About</Button>
+                    </Link>
+                    {!session ? (
+                        <Link href={"/api/auth/signin"}>
+                            <Button variant={"outline"} onClick={() => signIn("email", { callbackUrl: "/select", redirect: false })}>
+                                Signin
+                            </Button>
                         </Link>
-                       : 
-                       <UserAuth />
-                    }  
+                    ) : (
+                        <UserAuth />
+                    )}
+                </div>
             </div>
         </div>
-    </div>
+    );
 }
